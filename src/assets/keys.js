a93b9d6a12aa17345c52803d89e9fe15c9e0850b
Mubarak Odufade <91754014+mao-99@users.noreply.github.com>
2024-04-06 03:31:38 -0400

    Update keys.js

--- src/assets/keys.js
+++ src/assets/keys.js
@@ -1,3 +1,3 @@
-export const exchangeRateHost_apiKey = '3d31b381c3d4f84f1a235d9162579cfb';
+export const exchangeRateHost_apiKey = '';
 
 const colors = ['#e9f2f1', '#f3eeed', '#fcf7f5', '#cf463f', '#756c6b', '#3e323c', '#c9dad5', '#ddd5d0']
